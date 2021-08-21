eae86cb1922fce61db18b2f2fb69ea6d5f72007e
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2021-08-21 18:45:04 +0200

    Add a comment to char_to_str()

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/*
+ * Returns a dynamically allocated, null-terminated string of length 1
+ * containing the 'c' character.
+ */
 char * char_to_str(char c);
 
 #endif /* UTILS_H */
